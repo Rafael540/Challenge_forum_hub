ddea6b1f4dee35a7b17c18376501af719602f381
Rafael540 <alves540@hotmail.com>
2025-12-28 22:20:08 -0300

    primeiro commit

---
+++ src/test/java/Forum_Hub/demo/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package Forum_Hub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
